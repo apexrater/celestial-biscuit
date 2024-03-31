9d3579f7949980a69248206ade53ad9262cbd296
Kushagra Rawat <codemasterxyz@proton.me>
2024-03-31 16:14:36 +0530

    Final MVP

--- backend/api/index.js
+++ backend/api/index.js
@@ -21,7 +21,7 @@ AWS.config.update({
   region: process.env.REGION,
 });
 const route53 = new AWS.Route53();
-app.post("/register", async (req, res) => {
+app.post("/backend/api/register", async (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
   const { name, email, password } = req.body;
 
@@ -36,7 +36,7 @@ app.post("/register", async (req, res) => {
     res.status(422).json(e);
   }
 });
-app.post("/login", async (req, res) => {
+app.post("/backend/api/login", async (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
   const { email, password } = req.body;
   const userDoc = await User.findOne({ email });
@@ -62,10 +62,10 @@ app.post("/login", async (req, res) => {
     res.json("not found");
   }
 });
-app.post("/logout", (req, res) => {
+app.post("/backend/api/logout", (req, res) => {
   res.cookie("token", "").json(true);
 });
-app.get("/profile", (req, res) => {
+app.get("/backend/api/profile", (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
   const { token } = req.cookies;
   if (token) {
@@ -78,7 +78,7 @@ app.get("/profile", (req, res) => {
     res.json(null);
   }
 });
-app.get("/list-hosted-zones", async (req, res) => {
+app.get("/backend/api/list-hosted-zones", async (req, res) => {
   try {
     const data = await route53.listHostedZones().promise();
     res.json(data.HostedZones);
@@ -87,7 +87,7 @@ app.get("/list-hosted-zones", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-app.post("/create-hosted-zone", async (req, res) => {
+app.post("/backend/api/create-hosted-zone", async (req, res) => {
   try {
     const formData = req.body;
     console.log(formData);
@@ -112,7 +112,7 @@ app.post("/create-hosted-zone", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-app.post("/create-records", async (req, res) => {
+app.post("/backend/api/create-records", async (req, res) => {
   try {
     const { data, hostedId } = req.body;
 
